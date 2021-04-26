ec8adef12bebf34308ff52943ff1a72fcd0a2ca1
QuantumIsaac <quantumisaac@gmail.com>
2021-04-26 01:02:56 -0500

    fixed release script

--- scripts/git-release.bat
+++ scripts/git-release.bat
@@ -4,9 +4,6 @@ set /p VERSION=<"C:\Users\quant\eclipse-workspace\HeistMC\VERSION.txt"
 
 cd ..
 git add .
-echo "Committing"
 git commit -m "Bump to version %VERSION%"
-echo "Tagging..."
-git tag -a v%VERSION% "Bump to version %VERSION%"
-echo "Pushing..."
+git tag -a v%VERSION% -m "Bump to version %VERSION%"
 git push origin v%VERSION%
