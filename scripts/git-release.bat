0f4c4e369bfe544028f1abbe0a84fab94e1b49ec
QuantumIsaac <quantumisaac@gmail.com>
2021-04-26 01:39:21 -0500

    merged git scripts, HeistMC-latest.jar will now actually reflect latest build

--- scripts/git-release.bat
+++ scripts/git-release.bat
@@ -2,6 +2,22 @@
 
 set /p VERSION=<"C:\Users\quant\eclipse-workspace\HeistMC\VERSION.txt"
 
+rem prepare release JAR
+
+if exist "..\target\HeistMC-%VERSION%.jar" (
+    copy "..\target\HeistMC-%VERSION%.jar" "..\release"
+    if exist "..\release\HeistMC-latest.jar" (
+        del "..\release\HeistMC-latest.jar"
+    )
+    rename "..\release\HeistMC-%VERSION%.jar" "HeistMC-latest.jar"
+    echo Successfully prepared release JAR.
+) else (
+    echo No version '%1' was found.
+    goto:eof
+)
+
+rem commit, tag, and push
+
 cd ..
 git add .
 git commit -m "Bump to version %VERSION%"
